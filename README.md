1990284b3876efc5021455c96f0bb372aa42be24
Sagiruddin Mondal <sagir.sagir@gmail.com>
2015-08-07 16:07:18 +0530

    initial project

---
+++ README.md
@@ -0,0 +1,2 @@
+# play-scala-angularjs-architecture
+A combined architecture using play framework with scala and angularJS ~
---
+++ app/controllers/Application.scala
@@ -0,0 +1,27 @@
+package controllers
+
+import play.api._
+import play.api.libs.json.{Json, JsValue}
+import play.api.mvc._
+
+import play.api.Play
+import play.api.mvc._
+
+import scala.io.Source
+import play.api.libs.json._
+
+import scala.io.Source
+
+class Application extends Controller {
+
+  def index =  Action {
+    val projectRoot = Play.current.path
+    val addingPath = Seq(projectRoot, "/conf/json/result.json")
+    val finalPath = addingPath.mkString
+    val file = Source.fromFile(finalPath)
+    val mainFile = file.mkString
+
+    Ok(views.html.index(mainFile))
+  }
+
+}
