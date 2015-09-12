48c006877f151b217c85d0f08be3ea1a1018ee2f
Sagiruddin Mondal <sagir.sagir@gmail.com>
2015-09-12 14:31:16 +0530

    adding require js dependency

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := """play-scala-angularjs-architecture"""
+name := "play-scala-angularjs-architecture"
 
 version := "1.0-SNAPSHOT"
 
@@ -10,11 +10,19 @@ libraryDependencies ++= Seq(
   jdbc,
   cache,
   ws,
-  specs2 % Test
+  specs2 % Test,
+  "org.webjars" % "angularjs" % "1.3.0-beta.2",
+  "org.webjars" % "requirejs" % "2.1.11-1"
 )
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+pipelineStages := Seq(rjs, digest, gzip)
 
 resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
 
 // Play provides two styles of routers, one expects its actions to be injected, the
 // other, legacy style, accesses its actions statically.
 routesGenerator := InjectedRoutesGenerator
+
+
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,178 +1,35 @@
-angular.module('modelManager', ['ui.router', 'modelManager.controllers'])
+/*global require, requirejs */
 
-    .config(function($stateProvider, $compileProvider, $urlRouterProvider) {
-        $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|content|file):/);
-        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|content|file):/);
-        $stateProvider
+'use strict';
 
-            .state('about', {
-                url: "/about",
-                templateUrl: "ok.scala.html",
-               // controller: '/getTopNavBarData'
-            })
+requirejs.config({
+    paths: {
+        'angular': ['../lib/angularjs/angular'],
+        'angular-route': ['../lib/angularjs/angular-route']
+    },
+    shim: {
+        'angular': {
+            exports : 'angular'
+        },
+        'angular-route': {
+            deps: ['angular'],
+            exports : 'angular'
+        }
+    }
+});
 
+require(['angular', './controllers', './directives', './filters', './services', 'angular-route'],
+    function(angular, controllers) {
 
+        // Declare app level module which depends on filters, and services
 
-        $urlRouterProvider.otherwise('/dashboard');
+        angular.module('newNw', ['newNw.filters', 'newNw.services', 'newNw.directives', 'ngRoute']).
+            config(['$routeProvider', function($routeProvider) {
+                $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: controllers.MyCtrl1});
+                $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: controllers.MyCtrl2});
+                $routeProvider.otherwise({redirectTo: '/view1'});
+            }]);
+
+        angular.bootstrap(document, ['newNw']);
+
     });
-     /*
-.config(['$routeProvider', '$locationProvider',
-    function($routeProvider, $locationProvider) {
-        $routeProvider
-            .when('/index', {
-                templateUrl: '/'
-
-            })
-
-            .when('/about', {
-                templateUrl: '/getTopNavBarData'
-
-            })
-
-            .when('/contact', {
-                templateUrl: '/getTopNavBarData'
-
-            })
-            .when('/Book/:bookId/ch/:chapterId', {
-                templateUrl: 'chapter.html'
-            })
-
-            .otherwise({redirectTo: '/index'});
-        $locationProvider.html5Mode(true);
-    }]);
-
-angular.module('modelManager', ['ui.router', 'modelManager.controllers'])
-
-    .config(function($stateProvider, $compileProvider, $urlRouterProvider) {
-        $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|content|file):/);
-        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|content|file):/);
-        $stateProvider
-
-            .state('dashboard', {
-                url: "/dashboard",
-                templateUrl: "partials/dashboard.scala.html",
-                controller: 'dashboardCtrl'
-            })
-
-            .state('comparemodel', {
-                url: "/comparemodel",
-                templateUrl: "partials/comparemodel.scala.html",
-                controller: 'comparemodelCtrl'
-            })
-
-            .state('metadata', {
-                url: "/metadata",
-                templateUrl: "partials/metadata.scala.html",
-                controller: 'metadataCtrl'
-            })
-
-            .state('metadata.entitycfg', {
-                url: "/entitycfg",
-                templateUrl: "partials/entitycfg.scala.html",
-                controller: 'entitycfgCtrl'
-            })
-
-            .state('metadata.datasourcecfg', {
-                url: "/datasourcecfg",
-                templateUrl: "partials/datasourcecfg.scala.html",
-                controller: 'datasourcecfgCtrl'
-            })
-
-            .state('metadata.graphview', {
-                url: "/graphview",
-                templateUrl: "partials/graphview.scala.html",
-                controller: 'graphviewCtrl'
-            })
-
-            .state('entitydetail', {
-                url: "/entitydetail",
-                templateUrl: "partials/entitydetail.scala.html",
-                controller: 'entitydetailCtrl'
-            })
-
-            .state('modelbrowser', {
-                url: "/modelbrowser",
-                templateUrl: "partials/modelbrowser.scala.html",
-                controller: 'modelbrowserCtrl'
-            })
-
-            .state('modelbrowser.allmodels', {
-                url: "/allmodels",
-                templateUrl: "partials/modelviewer.scala.html",
-                controller: 'allmodelsCtrl'
-            })
-
-            .state('modelbrowser.mymodels', {
-                url: "/mymodels",
-                templateUrl: "partials/modelviewer.scala.html",
-                controller: 'mymodelsCtrl'
-            })
-
-            .state('modelbrowser.sharedmodels', {
-                url: "/sharedmodels",
-                templateUrl: "partials/modelviewer.scala.html",
-                controller: 'sharedmodelsCtrl'
-            })
-
-            .state('modelbuilder', {
-                url: "/modelbuilder",
-                templateUrl: "partials/modelbuilder.scala.html",
-                controller: 'modelbuilderCtrl'
-            })
-
-            .state('modelbuilder.createmodel', {
-                url: "/createmodel",
-                templateUrl: "partials/createmodel.scala.html",
-                controller: 'createmodelCtrl'
-            })
-
-            .state('modelbuilder.createtype3model', {
-                url: "/createmodel",
-                templateUrl: "partials/createmodel.scala.html",
-                controller: 'createtype3modelCtrl'
-            })
-
-            .state('adminsettings', {
-                url: "/adminsettings",
-                templateUrl: "partials/adminsettings.scala.html",
-                controller: 'adminsettingsCtrl'
-            })
-
-            .state('userView', {
-                url: "/userView",
-                templateUrl: "userView",
-                controller: 'userCtrl'
-            })
-
-            .state('userdetail',{
-                url: '/userdetail',
-                templateUrl: 'userDetail',
-                controller: 'userDetailCtrl'
-            })
-
-            .state('usergroupdetail',{
-                url: '/usergroupdetail',
-                templateUrl: 'userGroupDetail',
-                controller: 'userGroupdetailCtrl'
-            })
-
-            .state('setting',{
-                url: '/setting',
-                templateUrl: "partials/viewtable.scala.html",
-                controller: 'viewTableCtrl'
-            })
-
-            .state('logout',{
-                url: '/logout',
-                templateUrl: "logout"
-            })
-
-            .state('testReport',{
-                url: '/testReport',
-                templateUrl: "testReport",
-                controller: 'testReportCtrl'
-            });
-
-
-        $urlRouterProvider.otherwise('/dashboard');
-    });*/
